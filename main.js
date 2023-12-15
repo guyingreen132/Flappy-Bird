bd8bc678f09a0b28d310b8276db67bfcb949689a
Guyingreen <147945223+guyingreen132@users.noreply.github.com>
2023-12-14 18:49:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -8,11 +8,8 @@ function preload(){
 }
 
 function setup() {
-  var cnv = createCanvas(900, 1600);
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  cnv.position(x, y);
-  background(255, 0, 200);
+    var canvas = createCanvas(900, 1600);
+    canvas.parent('canvas'); //'canvas' is the id of the target HTML element
 }
 
 
